e572d5a93ef49bd28721908ce0ae139150ec578d
Chris <chris@clr.is>
2022-10-22 15:40:23 +0100

    Changed my mind already Using the .ignore extension on programs instead; just easier.

--- notesc/preproc.sh
+++ notesc/preproc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: preproc.sh
 
-m4 -P --define=m4__PYTHON3_PATH="$(which python3)" notesc.py.m4 > notesc.py
+m4 -P --define=m4__PYTHON3_PATH="$(which python3)" notesc.py > notesc.ignore
